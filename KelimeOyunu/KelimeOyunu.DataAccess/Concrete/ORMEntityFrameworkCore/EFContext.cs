1a31c050524d872e09c4186590a52b7fdced5e71
Hazar Arda Tüysüz <32832628+hazarardatuysuz@users.noreply.github.com>
2022-06-04 02:34:01 +0300

    word game project

---
+++ KelimeOyunu/KelimeOyunu.DataAccess/Concrete/ORMEntityFrameworkCore/EFContext.cs
@@ -0,0 +1,20 @@
+﻿using KelimeOyunu.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KelimeOyunu.DataAccess.Concrete.ORMEntityFrameworkCore
+{
+    public class EFContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-O1AASO9\SQLEXPRESS;Initial Catalog=kelimeoyunu;Integrated Security=True");
+        }
+
+        public DbSet<Kelime> Kelimeler { get; set; }
+        public DbSet<Oturum> Oturumlar { get; set; }
+        public DbSet<Surec> Surecler { get; set; }
+    }
+}
